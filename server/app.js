890f7b31b6b8fc4ba29a8a319aa53b6cd7f89af6
vanina bermejo <vanibermejo@gmail.com>
2016-05-23 11:42:27 -0300

    add mongo connection and index logic

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 "use strict";
 let express= require("express");
 let app = express();
+let mongoUtil =require("./mongoUtil");
+
+mongoUtil.connect();
 app.get('/sports', (request, response)=>{
-    response.json(["Weightlifting","Cycling", "Swimming"]);
+    let sports = mongoUtil.sports();
+    sports.find().toArray((err,docs)=>{
+        console.log(JSON.stringify(docs));
+        let sportNames = docs.map((sport) => sport.name);
+        response.json(sportNames);
+    });
 });
 
 
--- server/mongoUtil.js
+++ server/mongoUtil.js
@@ -1,21 +1,21 @@
 "use strict";
-
 let mongo = require('mongodb');
-let client = mongo.MongoClient;
+let client= mongo.MongoClient;
 let _db;
+module.exports ={
+    connect(){
+        client.connect('mongodb://localhost:27017/olympics-dev',(err, db) =>{
+            if(err){
+                console.log("Error connecting to mongo, check instance of mongod");
+                process.exit(1);
+            }
+            _db=db;
+            console.log("connected to mongodb");
+        
+        });
+    },
+    sports(){
+      return  _db.collection('sports');
+    }
 
-module.exports = {
-  connect() {
-    client.connect('mongodb://localhost:27017/olympics-dev', (err, db) => {
-      if(err) {
-        console.log("Error connecting to Mongo - check mongod connection");
-        process.exit(1);
-      }
-      _db = db;
-      console.log("Connected to Mongo");
-    });
-  },
-  sports(){
-    return _db.collection('sports');
-  }
 }
